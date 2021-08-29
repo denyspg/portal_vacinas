c9c498e1a2c9aa6bfbe4acc6b5ded775ef37137e
Lucas Peter Medeiros <lucasmedei@gmail.com>
2021-08-29 19:48:56 -0300

    Alert para mostrar mais infos

--- src/scripts/catalog.script.js
+++ src/scripts/catalog.script.js
@@ -1,3 +1,15 @@
-function showDescription(){
-    alert("aqui tem a descrição da vacina")
-}
+let infoBCG = document.getElementById("infoBCG")
+let infoH1N1 = document.getElementById("infoH1N1")
+let infoPfizer = document.getElementById("infoPfizer")
+
+infoBCG.addEventListener('click', () =>{
+    alert('A vacina BCG é dada quando se é bebezinho e fica marquinha no braço')
+})
+
+infoH1N1.addEventListener('click', () =>{
+    alert('A vacina H1N1 é dada para se proteger da gripe, muito importante')
+})
+
+infoPfizer.addEventListener('click', () =>{
+    alert('Ta passada?!')
+})
