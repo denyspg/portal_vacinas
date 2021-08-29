c143d84ce4d41e1adc10411de60284292bab5291
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2021-08-28 23:06:20 -0300

    remoção dos listeners do primeiro e ultimo nome.

--- webapp/components/login/login.controller.js
+++ webapp/components/login/login.controller.js
@@ -1,26 +1,11 @@
-let valorPrimeiroNome = document.getElementById("primeiroNome");
-let valorUltimoNome = document.getElementById("ultimoNome");
 let email = document.getElementById("email");
 
 
-valorPrimeiroNome.addEventListener("blur", function(event) {
+function verificaNomeBranco(){
     primeiroNome=document.querySelector('#primeiroNome').value;
     if (primeiroNome==''){
       alert("O campo não pode ficar vazio!");
     }
-  }, true);
+}
 
-
-valorUltimoNome.addEventListener("blur", function(event) {
-    ultimoNome=document.querySelector('#ultimoNome').value;
-    if (ultimoNome==''){
-      alert("O campo não pode ficar vazio!");
-    }
-  }, true);
-
-email.addEventListener("blur", function(event) {
-    email=document.querySelector('#ultimoNome').value;
-    if (email==''){
-      alert("O campo não pode ficar vazio!");
-    }
-}, true);
+email.addEventListener("blur", verificaNomeBranco, true);
