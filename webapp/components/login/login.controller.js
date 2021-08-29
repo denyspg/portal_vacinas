7edd36fd43ec9bbd1dedc5a916864cf8110ea500
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2021-08-29 13:23:52 -0300

    inclusão de event listener nos campos de nome

--- webapp/components/login/login.controller.js
+++ webapp/components/login/login.controller.js
@@ -1,11 +1,29 @@
 let email = document.getElementById("email");
+let primeiroNome = document.getElementById("primeiroNome");
+let ultimoNome = document.getElementById("ultimoNome");
 
 
-function verificaNomeBranco(){
-    primeiroNome=document.querySelector('#primeiroNome').value;
+function verificaPrimeiroNomeBranco(){
+   var primeiroNome=document.querySelector('#primeiroNome').value;
     if (primeiroNome==''){
       alert("O campo não pode ficar vazio!");
-    }
-}
+    } 
+};
 
-email.addEventListener("blur", verificaNomeBranco, true);
+function verificaUltimoNomeBranco(){
+var ultimoNome=document.querySelector('#ultimoNome').value;
+if (ultimoNome==''){
+  alert("O campo não pode ficar vazio!");
+}
+};
+
+function verificaEmailBranco(){
+var email=document.querySelector('#email').value;
+if (email==''){
+  alert("O campo não pode ficar vazio!");
+}
+};
+
+email.addEventListener("blur", verificaEmailBranco, true);
+primeiroNome.addEventListener("blur", verificaPrimeiroNomeBranco, true);
+ultimoNome.addEventListener("blur", verificaUltimoNomeBranco, true);
