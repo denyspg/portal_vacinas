6e3e9d207c8ab7a8534f12fda3de5c0f59a96f56
Denys Gaspar <53532689+dgaspar1@users.noreply.github.com>
2021-09-01 19:29:37 -0300

    fix: refactor catalogo

--- src/scripts/catalog.script.js
+++ src/scripts/catalog.script.js
@@ -1,30 +1,30 @@
-const infoBCG = document.getElementById("infoBCG")
-const infoPfizer = document.getElementById("infoPfizer")
-const infoH1N1 = document.getElementById("infoH1N1")
-const infoFebreAmarela = document.getElementById("infoFebreAmarela")
-const infoPolio = document.getElementById("infoPolio")
-const infoRotavirus = document.getElementById("infoRotavirus")
+const bancoDeDados = {
+    vacinas: {
+        'bcg': {
+            descricao: 'Descrição BCG'
+        },
+        'pfizer': {
+            descricao: 'Descrição Pfizer'
+        },
+        'h1n1': {
+            descricao: 'Descrição H1N1'
+        },
+        'febreAmarela': {
+            descricao: 'Descrição Febre Amarela'
+        },
+        'polio': {
+            descricao: 'Descrição Poliomielite'
+        },
+        'rotaVirus': {
+            descricao: 'Descrição Rota Virus'
+        }
+    }
+};
 
-infoBCG.addEventListener('click', () =>{
-    alert('A vacina BCG é dada quando se é bebezinho e fica marquinha no braço')
-})
-
-infoPfizer.addEventListener('click', () =>{
-    alert('Ta passada?!')
-})
-
-infoH1N1.addEventListener('click', () =>{
-    alert('A vacina H1N1 é dada para se proteger da gripe, muito importante')
-})
-
-infoFebreAmarela.addEventListener('click', () =>{
-    alert('Vacina muito importante pra não ter febre nem ficar amarelo')
-})
-
-infoPolio.addEventListener('click', () =>{
-    alert('Cadê o Zé Gotinha???')
-})
-
-infoRotavirus.addEventListener('click', () =>{
-    alert('Tem que tomar essa pra não dar dor de barriga')
-})
+function exibeDescricao (nomeVacina){
+    try{
+        alert(bancoDeDados.vacinas[nomeVacina].descricao);
+    }catch(err){
+        alert('Descrição não cadastrada ou vacina não encontrada.');
+    }
+}
