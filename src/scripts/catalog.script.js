02648665fa406eee3b8252f2373dd8753348ac90
Lucas Peter Medeiros <lucasmedei@gmail.com>
2021-08-31 21:42:38 -0300

    add mais vacinas e fix de redirect pra home

--- src/scripts/catalog.script.js
+++ src/scripts/catalog.script.js
@@ -1,15 +1,30 @@
 let infoBCG = document.getElementById("infoBCG")
-let infoH1N1 = document.getElementById("infoH1N1")
 let infoPfizer = document.getElementById("infoPfizer")
+let infoH1N1 = document.getElementById("infoH1N1")
+let infoFebreAmarela = document.getElementById("infoFebreAmarela")
+let infoPolio = document.getElementById("infoPolio")
+let infoRotavirus = document.getElementById("infoRotavirus")
 
 infoBCG.addEventListener('click', () =>{
     alert('A vacina BCG é dada quando se é bebezinho e fica marquinha no braço')
+})
+
+infoPfizer.addEventListener('click', () =>{
+    alert('Ta passada?!')
 })
 
 infoH1N1.addEventListener('click', () =>{
     alert('A vacina H1N1 é dada para se proteger da gripe, muito importante')
 })
 
-infoPfizer.addEventListener('click', () =>{
-    alert('Ta passada?!')
+infoFebreAmarela.addEventListener('click', () =>{
+    alert('Vacina muito importante pra não ter febre nem ficar amarelo')
+})
+
+infoPolio.addEventListener('click', () =>{
+    alert('Cadê o Zé Gotinha???')
+})
+
+infoRotavirus.addEventListener('click', () =>{
+    alert('Tem que tomar essa pra não dar dor de barriga')
 })
