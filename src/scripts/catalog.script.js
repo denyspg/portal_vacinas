4c024c088cbf65900fbc8ecef15d832e2c8661b2
Lucas Peter Medeiros <lucasmedei@gmail.com>
2021-09-20 22:12:02 -0300

    tentando exibir vacina na tela

--- src/scripts/catalog.script.js
+++ src/scripts/catalog.script.js
@@ -1,34 +1,3 @@
-const bancoDeDados = {
-    vacinas: {
-        'bcg': {
-            descricao: 'Descrição BCG'
-        },
-        'pfizer': {
-            descricao: 'Descrição Pfizer'
-        },
-        'h1n1': {
-            descricao: 'Descrição H1N1'
-        },
-        'febreAmarela': {
-            descricao: 'Descrição Febre Amarela'
-        },
-        'polio': {
-            descricao: 'Descrição Poliomielite'
-        },
-        'rotaVirus': {
-            descricao: 'Descrição Rota Virus'
-        }
-    }
-};
-
-function exibeDescricao (nomeVacina){
-    try{
-        alert(bancoDeDados.vacinas[nomeVacina].descricao);
-    }catch(err){
-        alert('Descrição não cadastrada ou vacina não encontrada.');
-    }
-}
-
 function pesquisaVacina() {
     const nomeVacina = document.getElementById('buscaVacina').value;
 
@@ -36,7 +5,7 @@ function pesquisaVacina() {
 	
 	const xhr = new XMLHttpRequest();
     if (nomeVacina == 'gripe' || nomeVacina == 'Gripe') {
-        xhr.open('GET', 'http://localhost:8080/ListaVacinas/Gripe');        
+        xhr.open('GET', 'http://localhost:8080/ListaVacinas/Gripe');
     }
 
     if (nomeVacina == 'polio' || nomeVacina == 'Poliomelite' || nomeVacina =='Polio') {
@@ -46,7 +15,7 @@ function pesquisaVacina() {
     if (nomeVacina == 'covid' || nomeVacina == 'Covid19' || nomeVacina == 'pfizer' || nomeVacina == 'Pfizer') {
         xhr.open('GET', 'http://localhost:8080/ListaVacinas/Pfizer');        
     }
-
+    xhr.setRequestHeader('Content-type', 'application/json')
 	xhr.onreadystatechange = () => {
 		if (xhr.readyState == 4) {
 			console.log(xhr.responseText);
@@ -58,3 +27,36 @@ function pesquisaVacina() {
         alert('É necessário preencher o nome da vacina!')
     }
 }
+function buscaTodasVacinas() {
+    	
+	const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:8080/ListaVacinas/');
+    xhr.setRequestHeader('Content-type', 'application/json')
+    xhr.responseType = 'json';
+	xhr.send();
+    xhr.onload = listaVacinas(xhr.response);
+}
+
+function listaVacinas(JsonObj) {
+    var vacinas = JsonObj['vacinas'];
+  
+    for (var i = 0; i < vacinas.length; i++) {
+      var myArticle = document.createElement('article');
+      var myH2 = document.createElement('h2');
+      var myPara1 = document.createElement('p');
+      var myPara2 = document.createElement('p');
+      var myPara3 = document.createElement('p');
+  
+      myH2.textContent = vacinas[i].name;
+      myPara1.textContent = 'Nome: ' + vacinas[i].nome;
+      myPara2.textContent = 'Descrição: ' + vacinas[i].descricao;
+  
+        
+      myArticle.appendChild(myH2);
+      myArticle.appendChild(myPara1);
+      myArticle.appendChild(myPara2);
+      myArticle.appendChild(myPara3);
+  
+      section.appendChild(myArticle);
+    }
+  }
